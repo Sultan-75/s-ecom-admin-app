7745d8c2de4c5a0d968adc8cba4f8063c90e45b7
Your Name <you@example.com>
2021-06-10 20:59:56 -0800

    initial commit

---
+++ src/components/Layout/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Header from "../Header/index";
+import { Col, Container, Row } from "react-bootstrap";
+import "./style.css";
+import { NavLink } from "react-router-dom";
+
+const Layout = (props) => {
+  return (
+    <>
+      <Header />
+      {props.sidebar ? (
+        <Container>
+          <Row>
+            <Col md={2} className="sidebar">
+              <ul>
+                <li>
+                  <NavLink exact to={`/`}>
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to={`/page`}>Page</NavLink>
+                </li>
+                <li>
+                  <NavLink to={`/category`}>Category</NavLink>
+                </li>
+                <li>
+                  <NavLink to={`/products`}>Products</NavLink>
+                </li>
+                <li>
+                  <NavLink to={`/orders`}>Orders</NavLink>
+                </li>
+              </ul>
+            </Col>
+            <Col md={10} style={{ marginLeft: "auto", paddingTop: "60px" }}>
+              {props.children}
+            </Col>
+          </Row>
+        </Container>
+      ) : (
+        props.children
+      )}
+    </>
+  );
+};
+
+export default Layout;
